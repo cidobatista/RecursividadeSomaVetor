dd0f52719db997111b31619b9c1f3d328c33622c
aparecidobatista <aparecido.batista@gmail.com>
2021-11-21 16:06:30 -0300

    Primeira Versao

---
+++ src/controller/VetorController.java
@@ -0,0 +1,25 @@
+package controller;
+
+public class VetorController {
+
+	public VetorController() {
+		super ();
+	}
+	
+	public int somaVetor(int [] vetor, int tamanho) {
+		//Condição de Parada
+		if (tamanho == 0) {
+			return 0;
+		} else {
+			int ultimaPosicao = tamanho -1;
+			int valor = vetor [ultimaPosicao];
+			tamanho = tamanho - 1;
+			return valor +somaVetor (vetor, tamanho);
+			
+	//		return vetor[tamanho -1] + somaVetor(vetor, tamanho -1);
+			
+		}
+	}
+	
+
+}
